e6879b6fb6d4c2cb7e37dec0ec71cf66f275851b
5eba5tian02 <165559035+5eba5tian02@users.noreply.github.com>
2024-05-07 12:24:07 +0200

    Update config_int.asm

--- code/ir_code_v2/config_int.asm
+++ code/ir_code_v2/config_int.asm
@@ -1,1 +1,126 @@
+;******************************************************************************
+;   This file is a basic template for assembly code for a PIC18F4525. Copy    *
+;   this file into your project directory and modify or add to it as needed.  *
+;                                                                             *
+;   Refer to the MPASM User's Guide for additional information on the         *
+;   features of the assembler.                                                *
+;                                                                             *
+;   Refer to the PIC18FX525/X620 Data Sheet for additional                    *
+;   information on the architecture and instruction set.                      *
+;                                                                             *
+;******************************************************************************
+;                                                                             *
+;    Filename:                                                                *
+;    Date:                                                                    *
+;    File Version:                                                            *
+;                                                                             *
+;    Author:                                                                  *
+;    Company:                                                                 *
+;                                                                             * 
+;******************************************************************************
+;                                                                             *
+;    Files Required: P18F4525.INC                                             *
+;                                                                             *
+;******************************************************************************
+
+	LIST P=18F4525	;directive to define processor
+	#include "P18F4525.INC"	;processor specific variable definitions
+
+;******************************************************************************
+;Configuration bits
+;Microchip has changed the format for defining the configuration bits, please 
+;see the .inc file for futher details on notation.  Below are a few examples.
+
+
+
+;   Oscillator Selection:
+    CONFIG	OSC = XT             ;LP
+    CONFIG WDT = OFF
+    CONFIG LVP = OFF
+
+;******************************************************************************
+;Variable definitions
+	
+
+;******************************************************************************
+;EEPROM data
+; Data to be programmed into the Data EEPROM is defined here
+
+		ORG	0xf00000
+
+		DE	"Test Data",0,1,2,3,4,5
+
+;******************************************************************************
+;Reset vector
+; This code will start executing when a reset occurs.
+
+		ORG	0x0000
+
+		goto	Main		;go to start of main code
+
+;******************************************************************************
+;High priority interrupt vector
+; This code will start executing when a high priority interrupt occurs or
+; when any interrupt occurs if interrupt priorities are not enabled.
+
+		ORG	0x0008
+
+		bra	HighInt		;go to high priority interrupt routine	
+
+
+
+
+
+        ORG 0x0018
+        
+        bra LowInt 
+
+;******************************************************************************
+;High priority interrupt routine
+; The high priority interrupt code is placed here to avoid conflicting with
+; the low priority interrupt vector.
+
+HighInt:
+
+;	*** high priority interrupt code goes here ***
+
+
+		retfie	FAST
+
+
+
+LowInt:
+
+        bcf PORTB, 1
+        bcf INTCON,INT0IF
+       
+        retfie Main
+;******************************************************************************
+;Start of main program
+; The main program code is placed here.
+
+Main:
+
+movlw 0x01
+movwf TRISB
+
+
+loop bsf PORTB, 1
+
+goto loop
+bsf INTCON,  INT0IE 
+bcf INTCON,  INT0IF ; Clears Flag
+bsf INTCON2, INTEDG0 ; sensitive on rising edge 
+bsf INTCON,  GIE ; Sets Globes interrupt Enable bit
+
+
+
+
+
+
+
+;******************************************************************************
+;End of program
+	goto $
+		END
 
